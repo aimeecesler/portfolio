39f1219a30120b4be70f18d20fa9a648768c0b61
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-02 01:27:21 -0500

    added black and white images to home

--- src/components/HomeImages/HomeImages.jsx
+++ src/components/HomeImages/HomeImages.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import kuduLeft from "../../images/kuduleft.png";
+import kuduRight from "../../images/kuduright.png";
 
 
 const HomeImages = () => {
   return (
     <div className="col-sm-12 text-center mt-5">
-
+      <Link to="/about">
+        <img
+          className="bw-kudu"
+          src={kuduLeft}
+          alt="kudu"
+        />
+      </Link>
+      <Link to="/portfolio">
+        <img
+          className="bw-kudu"
+          src={kuduRight}
+          alt="kudu"
+        />
+      </Link>
     </div>
   );
 };
