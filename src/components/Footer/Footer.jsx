bf17cf243c47c1283e153df121f659ed07c02558
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-02 21:39:12 -0500

    added formatting to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="footer mt-auto py-1">
       <div className="container-fluid pr-5 pl-5">
         <div className="row">
-          <span className="col-sm-4 text-left my-auto">
+          <span className="col-sm-4 text-center my-auto">
             <Link className="text-muted" to="/contact">
               Contact Me
             </Link>
@@ -16,7 +16,7 @@ const Footer = () => {
           <span className="col-sm-4 text-center copyright text-muted my-auto">
             Copyright&copy; 2020 Aimee Corbin Esler
           </span>
-          <span className="col-sm-4 text-right my-auto">
+          <span className="col-sm-4 text-center my-auto">
             <SocialLinks />
           </span>
         </div>
