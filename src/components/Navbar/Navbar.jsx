bbbcf97b70a5a714c70446ba234d9186997b32c4
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-02 21:06:26 -0500

    added social links to navbar and made it collapse all the time

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../images/ACElogosimple.png";
+import linkedin from "../../images/icons/linkedin.png";
+import github from "../../images/icons/github.png";
+import resume from "../../images/icons/resume.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-dark">
+      <nav className="navbar navbar-dark">
         <Link className="navbar-brand" to="/">
           <img src={logo} alt="logo" className="nav-logo" />
         </Link>
@@ -21,17 +24,37 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div
+          className="collapse navbar-collapse text-right"
+          id="navbarNavAltMarkup"
+        >
           <div className="navbar-nav ml-auto">
             <NavLink className="nav-link" to="/about">
               About Me
             </NavLink>
-            <NavLink className="nav-link" to="/contact">
-              Contact
-            </NavLink>
             <NavLink className="nav-link" to="/portfolio">
               Portfolio
             </NavLink>
+            <NavLink className="nav-link" to="/contact">
+              Contact
+            </NavLink>
+            <span>
+              <a
+                href="https://www.linkedin.com/in/aimee-esler-3bb31288/"
+                target="blank"
+              >
+                <img className="icon pr-2" src={linkedin} alt="LinkedIn Icon" />
+              </a>
+              <a href="https://github.com/aimeecesler" target="blank">
+                <img className="icon pr-2" src={github} alt="GitHub Icon" />
+              </a>
+              <a
+                href="https://drive.google.com/file/d/1YcDGV6925kBhLW2yzusRFPoqCfCV38Iu/view?usp=sharing"
+                target="blank"
+              >
+                <img className="icon pr-2" src={resume} alt="Resume Icon" />
+              </a>
+            </span>
           </div>
         </div>
       </nav>
