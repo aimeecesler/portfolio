d5d5760e8cbd56175eef709152b6c5af1aac3f65
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-02 20:50:37 -0500

    added contact link and formatting to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,10 +9,14 @@ const Footer = () => {
   return (
     <footer className="footer mt-auto py-1">
       <div className="container-fluid pr-5 pl-5">
-        <span className="text-muted">
+        <div className="row">
+        <span className="col-sm-4 text-left my-auto">
+          <Link className="text-muted" to="/contact">Contact Me</Link>
+        </span>
+        <span className="col-sm-4 text-center copyright text-muted my-auto">
           Copyright&copy; 2020 Aimee Corbin Esler
         </span>
-        <span className="float-right">
+        <span className="col-sm-4 text-right my-auto">
           <a
             href="https://www.linkedin.com/in/aimee-esler-3bb31288/"
             target="blank"
@@ -29,6 +33,7 @@ const Footer = () => {
             <img className="icon pr-2" src={resume} alt="Resume Icon" />
           </a>
         </span>
+        </div>
       </div>
     </footer>
   );
