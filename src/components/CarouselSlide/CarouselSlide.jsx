e2269955ec9075fda7c4024dd3cd71207061945d
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-04 17:16:09 -0500

    formatting and removed text on portfolio images

--- src/components/CarouselSlide/CarouselSlide.jsx
+++ src/components/CarouselSlide/CarouselSlide.jsx
@@ -1,23 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CarouselSlide = (props) => {
   return (
       <div className={props.class}>
+        <Link to="">
         <img
           src={props.src}
           className="carousel-image d-block w-100"
           alt={props.alt}
           data-interval="10000"
         />
-        <div className="carousel-caption d-none d-md-block">
-          <h5>{props.name}</h5>
-          <p>
-            <a href={props.github}>GitHub Repository</a>
-          </p>
-          <p>
-            <a href={props.deployed}>Deployed Application</a>
-          </p>
-        </div>
+        </Link>
       </div>
   );
 };
